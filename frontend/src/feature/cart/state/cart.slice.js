98c8a94ad7158b177aea136f08877ad672e50190
arunz6 <arunrajput88176894@gmail.com>
2026-07-21 20:05:20 +0530

    feat: implement cart state management and redux store integration

---
+++ frontend/src/feature/cart/state/cart.slice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartslice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+    loading: false,
+    error: null,
+  },
+  reducers: {
+    setitems: (state, action) => {
+      state.items = action.payload;
+    },
+    additem: (state, action) => {
+      state.items.push(action.payload);
+    },
+    removeitem: (state, action) => {},
+    updatequantity: (state, action) => {},
+    setloading: (state, action) => {
+      state.loading = action.payload;
+    },
+    seterror: (state, action) => {
+      state.error = action.payload;
+    },
+  },
+});
+
+export const { setcart, setloading, seterror, additem } = cartslice.actions;
+export default cartslice.reducer;
