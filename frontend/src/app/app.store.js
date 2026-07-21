399e18e6d5f14d17b27f6f32850ac0a912232ec8
arunz6 <arunrajput88176894@gmail.com>
2026-07-21 20:14:33 +0530

    feat: initialize Redux store with auth, product, and cart reducers

--- frontend/src/app/app.store.js
+++ frontend/src/app/app.store.js
@@ -1,11 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../feature/auth/state/auth.slice";
 import productReducer from "../feature/products/state/product.slice";
-import cartslice from "../feature/cart/state/cart.slice";
+import cartReducer from "../feature/cart/state/cart.slice";
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     product: productReducer,
-    cart: cartslice,
+    cart: cartReducer,
   },
 });
